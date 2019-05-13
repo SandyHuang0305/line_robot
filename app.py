fc467d57ef8e384188b53502bc4d6918199d79d4
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-05-13 18:54:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,13 +50,13 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, Sticker_Message)
         
-        elif '哭哭' in msg:
+    elif '哭哭' in msg:
         Sticker_Message = StickerSendMessage(
             package_id='11538',
             sticker_id='51626522'
         )
         line_bot_api.reply_message(event.reply_token, Sticker_Message)
-    return
+    
     if msg in ['hi', 'Hi', '安安']: #寫成清單
         r = '你好'
     elif msg in ['吃飯了嗎？', '你吃飯了嗎']:
